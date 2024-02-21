95c7fbc53b42a7d8097a0b675dadf16195f47d94
ZephaW <zwamala@gmail.com>
2024-02-21 16:25:26 +0300

    fixed errors in event listerners

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-
-});
+    var form = document.getElementById('emailForm');
+  
+    form.addEventListener('submit', function(event) {
+    
+    });
+  });
