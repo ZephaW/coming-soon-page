c5f70184d0c9e3ef5f68fd5e57d3b80f50fa6fde
ZephaW <zwamala@gmail.com>
2024-02-29 12:12:11 +0300

    changes to validation

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const email = document.getElementById('email')
-const form = document.getElementById('form')
+const email = document.getElementById('email');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
 
 form.addEventListener('submit', (e) => {
-
+  let messages = [];
   
-  e.preventDefault() //prevents form from submitting
-})
+  if (email.value === '') {
+    messages.push('Email address is required');
+  } 
+  
+  else if (!isValidEmail(email.value)) {
+    messages.push('Please enter a valid email address');
+  }
 
+  if (messages.length > 0) {
+    e.preventDefault(); // prevents form from submitting
+    errorElement.innerText = messages;
+  }
+});
+
+function isValidEmail(email) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;  // Regular expression for basic email validation
+  return emailRegex.test(email);
+}
