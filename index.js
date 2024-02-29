7d22eb6d3feb64f8daec37eaab16a4ad14bb1f93
ZephaW <zwamala@gmail.com>
2024-02-29 10:25:34 +0300

    initial changes made for form validation

--- index.js
+++ index.js
@@ -1,25 +1,9 @@
-// Attach event listener to form
-document.querySelector(".form").addEventListener("submit", handleFormSubmission);
+const email = document.getElementById('email')
+const form = document.getElementById('form')
 
-// function to handle form submission
-function handleFormSubmission(event) {
-  event.preventDefault(); //this prevents the default form submission behaviour
+form.addEventListener('submit', (e) => {
 
-  var email = document.getElementById("email").value;
-  var emailError = document.getElementById("email-error");
+  
+  e.preventDefault() //prevents form from submitting
+})
 
-  if (email.trim() === "") { // this checks if email is empty
-      emailError.textContent = "Please provide an email address.";
-      emailError.style.display = "block";
-      return; // this exits the function early to prevent form submission
-  }
-
-  var emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/; //expression to check email format
-  if (!emailFormat.test(email)) { // If the email format is incorrect, display an error message
-      emailError.textContent = "Please provide a valid email address.";
-      emailError.style.display = "block";
-      return; // Exit the function early to prevent form submission
-  }
-
-  emailError.style.display = "none"; //if everything is valid, hide the error message and submit the form
-}
